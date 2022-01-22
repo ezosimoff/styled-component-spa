49b67df10f3e82e233a1f5293ba78530eebf66fe
zosimoff <ezosimoff@yandex.ru>
2022-01-23 02:12:16 +0300

    add details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,48 @@
+import axios from 'axios';
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { searchByCountry } from '../config';
+import DetailsInfo from '../components/DetailsInfo';
+
+const Button = styled.button`
+	border: none;
+	border-radius: var(--radius);
+	background-color: var(--ui-base-color);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 5px;
+	width: 100px;
+	box-shadow: var(--shadow);
+	cursor: pointer;
+
+	& svg {
+		margin-right: 7px;
+	}
+`;
 
 const Detailspage = () => {
-	return <div>Details Page</div>;
+	const [country, setCountry] = React.useState({});
+	const params = useParams();
+
+	React.useEffect(() => {
+		axios.get(searchByCountry(params.name)).then(({ data }) => {
+			setCountry(data[0]);
+		});
+	}, [params.name]);
+
+	const navigate = useNavigate();
+	return (
+		<>
+			{console.log('render')}
+			<Button onClick={() => navigate(-1)}>
+				<FaArrowLeft /> Back
+			</Button>
+			<DetailsInfo />
+		</>
+	);
 };
 
 export default Detailspage;
